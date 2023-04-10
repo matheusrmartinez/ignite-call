f664f332bb195b5db7f35c84c377353a1660f1ef
Matheus Martinez <matheus.martinez@ctw.bmwgroup.com>
2023-04-10 21:09:51 +0100

    Feature: Added custom Prisma Adapter

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -2,6 +2,8 @@ import { AuthScope, Scope } from "@/enums/authScopes";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { Routes } from "@/enums/routes";
+import { PrismaAdapter } from "@/lib/auth/prisma-adapter";
+import { NextApiRequest, NextApiResponse } from "next";
 
 const scopes = {
   email: AuthScope.email,
@@ -9,27 +11,35 @@ const scopes = {
   calendar: AuthScope.calendar,
 };
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
-      authorization: {
-        params: {
-          scope: Object.values(scopes).join(" "),
+export const buildNextAuthOptions = (
+  req: NextApiRequest,
+  res: NextApiResponse
+): NextAuthOptions => {
+  return {
+    adapter: PrismaAdapter(req, res),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+        authorization: {
+          params: {
+            scope: Object.values(scopes).join(" "),
+          },
         },
-      },
-    }),
-  ],
-  callbacks: {
-    async signIn({ account }) {
-      if (!account?.scope?.includes(scopes[Scope.calendar])) {
-        return `${Routes.connectCalendar}/?error=permissions`;
-      }
+      }),
+    ],
+    callbacks: {
+      async signIn({ account }) {
+        if (!account?.scope?.includes(scopes[Scope.calendar])) {
+          return `${Routes.connectCalendar}/?error=permissions`;
+        }
 
-      return true;
+        return true;
+      },
     },
-  },
+  };
 };
 
-export default NextAuth(authOptions);
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return NextAuth(req, res, buildNextAuthOptions(req, res));
+}
